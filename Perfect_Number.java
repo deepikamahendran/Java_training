972f9449fb2c025e8609a47e593cbd76355f4307
DeepikaMahendran <167070954+deepikamahendran@users.noreply.github.com>
2025-02-14 22:53:07 +0530

    Create Perfect_Number.java

---
+++ Perfect_Number.java
@@ -0,0 +1,43 @@
+package assesment;
+
+import java.util.Scanner;
+
+public class perfect_Number 
+{
+	public static int sumOfDivisors(int num) 
+	{
+	        int sum = 0;
+	        for (int i = 1; i <= num / 2; i++) 
+	        {
+	            if (num % i == 0) 
+	            {
+	                sum =sum + i;
+	            }
+	        }
+	        return sum;
+	}
+	    public static String checkNumberType(int num) 
+	    {
+	        int sum = sumOfDivisors(num);
+	        if (sum == num) 
+	        {
+	            return "Perfect Number";
+	        }
+	        else if (sum > num) 
+	        {
+	            return "Abundant Number";
+	        }
+	        else 
+	        {
+	            return "Deficient Number";
+	        }
+	    }
+
+	    public static void main(String[] args) 
+	    {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int num = scanner.nextInt();
+	        System.out.println(num + " is a " + checkNumberType(num));
+	    }
+}
